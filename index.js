46276e708091ae3119bc23fcc27c71745695cdb7
Wilhelm Klopp <wil.klopp@gmail.com>
2018-07-19 21:48:38 +0800

    Add safety

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const startChaos = require('./lib/start-chaos');
+const { createSafetyPR, isSafe } = require('./lib/safety');
 
 function comesFromChaosMonkey(context) {
   if (
@@ -13,9 +14,24 @@ function comesFromChaosMonkey(context) {
 
 module.exports = async (app) => {
   app.on('*', async (context) => {
-    if (comesFromChaosMonkey(context)) {
+    const { owner, repo } = context.repo();
+    if (comesFromChaosMonkey(context) || !await isSafe({ owner, repo, github: context.github })) {
       return;
     }
     await startChaos({ ...context.repo(), github: context.github });
+  });
+  app.on('installation_repositories.added', (context) => {
+    // intentionally only select this one
+    const repository = context.payload.repositories_added[0];
+    const { github } = context;
+    const { installation } = context.payload;
+    return createSafetyPR({ owner: installation.account.login, repo: repository.name, github });
+  });
+  app.on('installation.created', (context) => {
+    // intentionally only select this one
+    const repository = context.payload.repositories.repositories_added[0];
+    const { github } = context;
+    const { installation } = context.payload;
+    return createSafetyPR({ owner: installation.account.login, repo: repository.name, github });
   });
 };
